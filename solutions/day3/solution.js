a8b0f2e432f0ddef6e5e30504db942b8e543ff52
John Beech <johnbeech.github@mkv25.net>
2020-12-03 09:11:13 +0000

    Refactor and solve day3 for second star

--- solutions/day3/solution.js
+++ solutions/day3/solution.js
@@ -10,16 +10,12 @@ async function run () {
   await solveForSecondStar(input)
 }
 
-function clone (obj) {
-  return JSON.parse(JSON.stringify(obj))
+const TILES = {
+  TREE: '#',
+  SNOW: '.'
 }
 
-async function solveForFirstStar (input) {
-  const TILES = {
-    TREE: '#',
-    SNOW: '.'
-  }
-
+function createSlopeMap (input) {
   const slopeMap = {}
   slopeMap.slice = input.split('\n').map(line => line.split(''))
   slopeMap.width = slopeMap.slice[0].length
@@ -27,26 +23,59 @@ async function solveForFirstStar (input) {
   slopeMap.getPosition = (x, y) => {
     return slopeMap.slice[y][x % slopeMap.width]
   }
+  return slopeMap
+}
 
+function traverse (slopeMap, { dx, dy }) {
   const position = { x: 0, y: 0 }
-  const route = [clone(position)]
+  const route = []
 
   do {
     const location = slopeMap.getPosition(position.x, position.y)
     route.push({ x: position.x, y: position.y, tile: location })
-    position.x = position.x + 3
-    position.y = position.y + 1
+    position.x = position.x + dx
+    position.y = position.y + dy
   } while (position.y < slopeMap.height)
 
-  console.log('Encounters:', route)
+  return route
+}
 
-  const solution = route.filter(n => n.tile === TILES.TREE).length
+function countEncounters (route) {
+  return route.filter(n => n.tile === TILES.TREE).length
+}
+
+async function solveForFirstStar (input) {
+  const slopeMap = createSlopeMap(input)
+
+  const route = traverse(slopeMap, { dx: 3, dy: 1 })
+
+  console.log('Encounters:', route)
+  const solution = countEncounters(route)
 
   report('Solution 1:', solution, 'trees encountered out of', route.length, 'traversals')
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const slopeMap = createSlopeMap(input)
+
+  const routes = [
+    { dx: 1, dy: 1 },
+    { dx: 3, dy: 1 },
+    { dx: 5, dy: 1 },
+    { dx: 7, dy: 1 },
+    { dx: 1, dy: 2 }
+  ]
+
+  const traversals = routes.map(route => traverse(slopeMap, route))
+  console.log('Traversals', traversals)
+
+  const encounters = traversals.map(countEncounters)
+  console.log('Encounters', encounters)
+
+  const solution = encounters.reduce((acc, item) => {
+    return acc * item
+  }, 1)
+
   report('Solution 2:', solution)
 }
 
