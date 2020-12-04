01e989886b0b257636efa805112ec264971d16c0
John Beech <johnbeech.github@mkv25.net>
2020-12-04 08:06:13 +0000

    Allowing too many passports through

--- solutions/day4/solution.js
+++ solutions/day4/solution.js
@@ -31,9 +31,65 @@ async function solveForFirstStar (input) {
   report('Solution 1:', solution)
 }
 
+function validateHeight (height) {
+  const matches = height.match(/(\d+)([A-z]+)/)
+  if (!matches) {
+    return false
+  }
+
+  const [, value, unit] = matches
+  if (unit === 'cm') {
+    return value >= 150 && value <= 193
+  }
+  if (unit === 'in') {
+    return value >= 59 && value <= 76
+  }
+  return false
+}
+
+function validateHairColor (hairColor) {
+  return /#[0-9a-f]{6}/.test(hairColor)
+}
+function validateEyeColor (hairColor) {
+  return [
+    'amb',
+    'blu',
+    'brn',
+    'gry',
+    'grn',
+    'hzl',
+    'oth'
+  ].includes(hairColor)
+}
+
+function validatePassportId (id) {
+  return /[0-9]{9}/.test(id)
+}
+
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
-  report('Solution 2:', solution)
+  const passports = input.split('\n\n').filter(n => n).map(parsePassportRecord)
+
+  console.log(passports)
+  console.log('Found', passports.length, 'records in file')
+
+  const validPassports = passports.filter(p => {
+    const expcetedFields = p.byr && p.iyr && p.eyr && p.hgt && p.hcl && p.ecl && p.pid
+    if (!expcetedFields) {
+      return false
+    }
+
+    const validBirthYear = Number.parseInt(p.byr) >= 1920 && Number.parseInt(p.byr) <= 2020
+    const validIssueYear = Number.parseInt(p.iyr) >= 2010 && Number.parseInt(p.iyr) <= 2020
+    const validExpirationYear = Number.parseInt(p.eyr) >= 2020 && Number.parseInt(p.eyr) <= 2030
+    const validHeight = validateHeight(p.hgt)
+    const validHairColor = validateHairColor(p.hcl)
+    const validEyeColor = validateEyeColor(p.ecl)
+    const validPassportId = validatePassportId(p.pid)
+
+    return validBirthYear && validIssueYear && validExpirationYear && validHeight && validHairColor && validEyeColor && validPassportId
+  })
+  const solution = validPassports.length
+  report('Solution 1:', solution)
 }
 
 run()
