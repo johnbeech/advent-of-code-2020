b561c671d214a145a9680927ed6422c69d27bebe
John Beech <johnbeech.github@mkv25.net>
2020-12-07 07:41:14 +0000

    Incorrect solution for day5 first star

---
+++ solutions/day7/solution.js
@@ -0,0 +1,114 @@
+const path = require('path')
+const { read, write, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+async function run () {
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+function parseBag (line) {
+  // 1 drab beige bag
+  report('Parse Bag', line)
+  const [, quantity, style, color] = line.match(/(\d+)\s([a-z]+)\s([a-z]+)\sbag/)
+  const key = `${style}:${color}`
+  const bag = {
+    key,
+    style,
+    color,
+    quantity: Number.parseInt(quantity)
+  }
+  report('Parse bag', line, ':', bag)
+  return bag
+}
+
+function parseRule (line) {
+  // vibrant red bags contain 1 drab beige bag.
+  // striped beige bags contain no other bags.
+  const [left, right] = line.replace('.', '').split(' bags contain ')
+  const bagContainer = left
+  const [style, color] = bagContainer.split(' ')
+  const key = `${style}:${color}`
+  if (right === 'no other bags') {
+    return {
+      key,
+      style,
+      color,
+      contains: []
+    }
+  } else {
+    const contains = right.split(', ').map(parseBag)
+    return {
+      key,
+      style,
+      color,
+      contains
+    }
+  }
+}
+
+function leafNodesFirst (a, b) {
+  const an = a.contains.length
+  const bn = b.contains.length
+  return an - bn
+}
+
+function mapBagHierarchy (rules) {
+  const tree = {}
+  rules.sort(leafNodesFirst).forEach(rule => {
+    const node = tree[rule.key] ? tree[rule.key] : { parents: [] }
+    node.rule = rule
+    tree[rule.key] = node
+    rule.contains.forEach(bag => {
+      const child = tree[bag.key] ? tree[bag.key] : { parents: [] }
+      child.parents.push(rule.key)
+      tree[bag.key] = child
+    })
+  })
+  return tree
+}
+
+function findParents (key, hierarchy, result) {
+  const node = hierarchy[key]
+  if (node.parents.length === 0) {
+    const { key, style, color } = node.rule
+    result.push({ key, style, color })
+  } else {
+    node.parents.forEach(parent => {
+      findParents(parent, hierarchy, result)
+    })
+  }
+  return result
+}
+
+async function solveForFirstStar (input) {
+  const rules = input.split('\n').filter(n => n).map(parseRule)
+
+  await write(fromHere('rules.json'), JSON.stringify(rules, null, 2), 'utf8')
+
+  const hierarchy = mapBagHierarchy(rules)
+
+  await write(fromHere('hierarchy.json'), JSON.stringify(hierarchy, null, 2), 'utf8')
+
+  console.log(hierarchy['shiny:gold'])
+
+  const topLevelParents = findParents('shiny:gold', hierarchy, [])
+
+  await write(fromHere('shiny-gold-parents.json'), JSON.stringify(topLevelParents, null, 2), 'utf8')
+
+  const uniqueBagColours = Array.from(new Set(topLevelParents.map(n => n.color)))
+  report('Unique Bag Colours', uniqueBagColours)
+
+  const solution = uniqueBagColours.length
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()
