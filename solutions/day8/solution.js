0bb9cdb805777cc1d3cb82bbbd7511ab667204c6
John Beech <johnbeech.github@mkv25.net>
2020-12-08 08:32:05 +0000

    Solve day8 for second star

--- solutions/day8/solution.js
+++ solutions/day8/solution.js
@@ -41,33 +41,61 @@ function computeJmp ({ program, acc, pos }) {
 function compute ({ program, acc, pos }, history) {
   const op = program[pos]
   history.push({ code: op.code, value: op.value, acc, pos })
-  report('Computing', history[history.length - 1])
+  // report('Computing', history[history.length - 1])
   return operations[op.code]({ program, acc, pos })
+}
+
+function runComputer (program) {
+  let computer = { program, acc: 0, pos: 0 }
+  let status = 'running'
+  const history = []
+  const computedCodes = {}
+  do {
+    computedCodes[computer.pos] = (computedCodes[computer.pos] || 0) + 1
+    if (!program[computer.pos]) {
+      report('Terminating program; no operation found at:', computer.pos, 'of program length:', program.length)
+      status = 'terminated'
+    } else if (computedCodes[computer.pos] === 1) {
+      computer = compute(computer, history)
+    } else {
+      report('Infite loop detected; breaking program at:', computer.pos, 'after', history.length, 'computations.')
+      status = 'infinite loop'
+    }
+  } while (status === 'running')
+
+  return { computer, history, computedCodes, status }
 }
 
 async function solveForFirstStar (input) {
   const program = input.split('\n').filter(n => n).map(parseOpCode)
 
-  let computer = { program, acc: 0, pos: 0 }
-  let running = true
-  const history = []
-  const computedCodes = {}
-  do {
-    computedCodes[computer.pos] = (computedCodes[computer.pos] || 0) + 1
-    if (computedCodes[computer.pos] === 1) {
-      computer = compute(computer, history)
-    } else {
-      report('Infite loop detected; breaking program at:', computer, 'after', history.length, 'computations.')
-      running = false
-    }
-  } while (running)
+  const { computer } = runComputer(program)
 
   const solution = computer.acc
   report('Solution 1:', solution)
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const program = input.split('\n').filter(n => n).map(parseOpCode)
+
+  const possibleCodes = program.filter((op, index) => {
+    const nopOrJmp = op.code === 'nop' || op.code === 'jmp'
+    op.pos = index
+    return nopOrJmp
+  })
+
+  report('Possible codes', possibleCodes)
+
+  const computations = possibleCodes.map(op => {
+    const modifiedProgram = JSON.parse(JSON.stringify(program))
+    modifiedProgram[op.pos].code = op.code === 'jmp' ? 'nop' : 'jmp'
+    return runComputer(modifiedProgram)
+  })
+
+  const terminations = computations.filter(n => n.status === 'terminated')
+  report('Found', terminations.length, 'terminated programs out of', computations.length, 'computations.')
+
+  const solution = terminations[0].computer.acc
   report('Solution 2:', solution)
 }
 
