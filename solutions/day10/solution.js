0e87f685d38ad8c73115011314569ca719955947
John Beech <johnbeech.github@mkv25.net>
2020-12-10 08:29:43 +0000

    Incorrect solution for day10

---
+++ solutions/day10/solution.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const { read, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+async function run () {
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+function createAdapter (rating) {
+  rating = Number.parseInt(rating)
+  return {
+    rating,
+    compatability: [
+      rating + 1,
+      rating + 2,
+      rating + 3
+    ]
+  }
+}
+
+function connectAdapter (adapter, index, adapters) {
+  const { rating, compatability } = adapter
+  const connections = adapters.filter(n => compatability.includes(n.rating))
+  const connection = connections.length > 0 ? connections[0].rating : rating + 3
+  const jolt = connection - rating
+  return {
+    rating,
+    connections: connections.length,
+    connection,
+    jolt
+  }
+}
+
+function sortByAdapterRating (a, b) {
+  const ar = a.rating
+  const br = b.rating
+  return ar - br
+}
+
+async function solveForFirstStar (input) {
+  const adapters = input.split('\n').filter(n => n).map(createAdapter).sort(sortByAdapterRating)
+
+  const connectedAdapters = adapters.map(connectAdapter)
+
+  report('Connected', connectedAdapters)
+
+  const jolt1Connections = connectedAdapters.filter(n => n.jolt === 1)
+  const jolt3Connections = connectedAdapters.filter(n => n.jolt === 3)
+
+  const solution = jolt1Connections.length * jolt3Connections.length
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()
