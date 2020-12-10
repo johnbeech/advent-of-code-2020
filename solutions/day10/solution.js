938747710f328feed9bc1855d63bb0d1c5f464d2
John Beech <johnbeech.github@mkv25.net>
2020-12-10 08:33:07 +0000

    Solve day10 for first star; off by one error

--- solutions/day10/solution.js
+++ solutions/day10/solution.js
@@ -51,8 +51,8 @@ async function solveForFirstStar (input) {
   const jolt1Connections = connectedAdapters.filter(n => n.jolt === 1)
   const jolt3Connections = connectedAdapters.filter(n => n.jolt === 3)
 
-  const solution = jolt1Connections.length * jolt3Connections.length
-  report('Solution 1:', solution)
+  const solution = (jolt1Connections.length + 1) * jolt3Connections.length
+  report('Solution 1:', solution, '1 Jolts:', (jolt1Connections.length + 1), '3 Jolts:', jolt3Connections.length)
 }
 
 async function solveForSecondStar (input) {
