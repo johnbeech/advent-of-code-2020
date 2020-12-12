2de5ecc49840e5286edef859180da012ffd2d98b
John Beech <johnbeech.github@mkv25.net>
2020-12-12 23:41:53 +0000

    Incorrect solution for day12 second star; test input calculates ok.

--- solutions/day12/solution.js
+++ solutions/day12/solution.js
@@ -20,7 +20,6 @@ const offsets = {
 const directions = Object.keys(offsets)
 
 function parseDirection (line) {
-  console.log('LINE:', line)
   const [, action, value] = line.match(/([NESWLRF])(\d+)/)
   return {
     action,
@@ -50,8 +49,8 @@ function move (ship, instruction) {
 function forward (ship, instruction) {
   const offset = offsets[ship.direction]
   return {
-    x: ship.x + offset.x * instruction.value,
-    y: ship.y + offset.y * instruction.value,
+    x: ship.x + (offset.x * instruction.value),
+    y: ship.y + (offset.y * instruction.value),
     direction: ship.direction
   }
 }
@@ -68,24 +67,86 @@ function turn (ship, instruction) {
   }
 }
 
-function drive (ship, instruction) {
-  const action = actions[instruction.action]
-  console.log('Ship:', ship.x, ',', ship.y, 'facing:', ship.direction, '- next instruction:', instruction.action, instruction.value)
-  return action(ship, instruction)
+function drive (actions) {
+  return (ship, instruction) => {
+    const action = actions[instruction.action]
+    if (ship.direction) {
+      console.log('Ship:', ship.x, ',', ship.y, 'facing:', ship.direction, '- next instruction:', instruction.action, instruction.value)
+    }
+    if (ship.wx && ship.wy) {
+      console.log('Ship:', ship.x, ',', ship.y, 'Waypoint:', ship.wx, ',', ship.wy, '- next instruction:', instruction.action, instruction.value)
+    }
+    return action(ship, instruction)
+  }
 }
 
 async function solveForFirstStar (input) {
   const instructions = input.split('\n').filter(n => n).map(parseDirection)
 
   const startPosition = { x: 0, y: 0, direction: 'E' }
-  const finalPosition = instructions.reduce(drive, startPosition)
+  const finalPosition = instructions.reduce(drive(actions), startPosition)
 
   const solution = Math.abs(finalPosition.x + finalPosition.y)
   report('Solution 1:', solution)
 }
 
+const waypointActions = {
+  N: moveWaypoint,
+  E: moveWaypoint,
+  S: moveWaypoint,
+  W: moveWaypoint,
+  L: turnWaypoint,
+  R: turnWaypoint,
+  F: forwardToWaypoint
+}
+
+function moveWaypoint (ship, instruction) {
+  return {
+    x: ship.x,
+    y: ship.y,
+    wx: ship.wx + (instruction.offset.x * instruction.value),
+    wy: ship.wy + (instruction.offset.y * instruction.value)
+  }
+}
+
+function turnWaypoint (ship, instruction) {
+  const turnDirection = instruction.action === 'L' ? -1 : 1
+  let turnSteps = ((instruction.value / 90) * turnDirection + 4) % 4
+  let wx = ship.wx
+  let wy = ship.wy
+  while (turnSteps > 0) {
+    turnSteps--
+    const owx = -wy
+    const owy = wx
+    wy = owx
+    wx = owy
+  }
+  return {
+    x: ship.x,
+    y: ship.y,
+    wx,
+    wy
+  }
+}
+
+function forwardToWaypoint (ship, instruction) {
+  const x = ship.x + (instruction.value * ship.wx)
+  const y = ship.y + (instruction.value * ship.wy)
+  return {
+    x,
+    y,
+    wx: ship.wx,
+    wy: ship.wy
+  }
+}
+
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const instructions = input.split('\n').filter(n => n).map(parseDirection)
+
+  const startPosition = { x: 0, y: 0, wx: 10, wy: -1 }
+  const finalPosition = instructions.reduce(drive(waypointActions), startPosition)
+
+  const solution = Math.abs(finalPosition.x + finalPosition.y)
   report('Solution 2:', solution)
 }
 
